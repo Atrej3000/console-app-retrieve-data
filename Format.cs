f7fd546371fbed5edf007146d9380ac77ab2d2a2
andre <andre@DESKTOP-OQC2B0M>
2022-10-21 10:14:39 +0300

    Add using for disposing HttpClient; Add changes to convert products from anonymous type to Dictionary; Refactor formatting in table logic;

--- Format.cs
+++ Format.cs
@@ -1,10 +1,27 @@
-﻿internal class Format
+﻿using System.Collections;
+using System.Text;
+
+internal class FormatTable
 {
-    public const int PADDING = -20;
-    public const string BORDER = "|----------" +
-                                  "----------" +
-                                  "----------" +
-                                  "----------" +
-                                  "-|";
+    private const int _padding = -20;
+    private const string _border = "|----------" +
+                                    "----------" +
+                                    "----------" +
+                                    "----------" +
+                                    "-|";
+    public static void MakeTable(Dictionary<string, string> products)
+    {
+        string topColumns = $"|{"Product name",_padding}" +
+                        $"|{"Category name",_padding}|";
+        StringBuilder sb = new();
+        sb.AppendLine(topColumns);
+        sb.AppendLine(_border);
+        foreach (var product in products)
+        {
+            sb.AppendLine($"|{product.Key,_padding}|{product.Value,_padding}|");
+            sb.AppendLine(_border);
+        }
+        Console.WriteLine(sb.ToString());
+    }
 }
 
--- Program.cs
+++ Program.cs
@@ -1,21 +1,15 @@
 ﻿using Newtonsoft.Json;
-using System.Text;
-using static Format;
+using static FormatTable;
 
 string url = "http://tester.consimple.pro";
-var client = new HttpClient();
+using var client = new HttpClient();
 var response = await client.GetStringAsync(url);
 Root? root = JsonConvert.DeserializeObject<Root>(response);
-var result = root?.Products.Select(p => new { Product = p.Name, Category = root.Categories.Single(c => c.Id == p.CategoryId).Name });
-if(result != null)
+var products = root?.Products
+    .Select(p => new { Product = p.Name, Category = root.Categories
+    .Single(c => c.Id == p.CategoryId).Name })
+    .ToDictionary(e => e.Product, e => e.Category);
+if (products != null)
 {
-    StringBuilder sb = new StringBuilder($"|{"Product name", PADDING}|{"Category name", PADDING}|\n");
-    sb.AppendLine(BORDER);
-	foreach (var item in result)
-	{
-		sb.AppendLine($"|{item.Product, PADDING}|{item.Category, PADDING}|");
-		sb.AppendLine(BORDER);
-	}
-	Console.WriteLine(sb.ToString());
+    MakeTable(products);
 }
-
